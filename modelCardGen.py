118719efe5e67b943758b829ba20696509af3611
Hoang Ha <64120343+hahuyhoang411@users.noreply.github.com>
2023-12-11 17:38:03 +0700

    Update modelCardGen

--- modelCardGen.py
+++ modelCardGen.py
@@ -1,47 +1,84 @@
-import fire
 import os
+import yaml
+import fire
 
-def parseMetadata(contents):
-    metadata = {}
-    lines = contents.split('\n')
-    in_metadata = False
-    for line in lines:
-        if line.strip() == '---':
-            if in_metadata:
-                break
-            else:
-                in_metadata = True
-                continue
-        if in_metadata and ':' in line:
-            key, value = line.split(':', 1)
-            metadata[key.strip()] = value.strip()
-    return metadata
+def modify_metadata(model_id):
+    """
+    Modify the metadata of the README.md based on the model_id.
+    Args:
+    model_id (str): The model identifier in the format 'creator/name'.
+    Returns:
+    str: Updated YAML metadata as a string.
+    """
+    model_creator, model_name = model_id.split('/')
 
-model_id = os.getenv('MODEL_ID')
-model_creator, model_name = model_id.split('/')
-
-def modelCardGen():
-    # Read README.md
+    # Read the original README.md content
     with open(f'./${{ env.MODEL_NAME }}/README.md', 'r') as file:
-        contents = file.read()
+        content = file.read()
 
-    # Parse existing metadata
-    metadata = parse_metadata(contents)
+    # Extract the YAML part from the content
+    yaml_part = content.split('---')[1]
 
-    # Update metadata
-    metadata['base_model'] = metadata.get('base_model', model_id)
-    metadata['model_creator'] = metadata.get('model_creator', model_creator)
-    metadata['model_name'] = metadata.get('model_name', model_name)
-    metadata['quantized_by'] = metadata.get('quantized_by', 'Jan')
-    metadata['language'] = metadata.get('language', '- en')
-    metadata['pipeline_tag'] = metadata.get('pipeline_tag', 'conversational')
+    # Parse the YAML content
+    data = yaml.safe_load(yaml_part)
 
-    # Reconstruct README
-    updated_readme = '---\n' + '\n'.join(f'{k}: {v}' for k, v in metadata.items()) + '\n---\n' + contents.split('---\n', 2)[-1]
+    # Update the YAML data with dynamic values
+    data['base_model'] = model_id
+    data['model_creator'] = model_creator
+    data['model_name'] = model_name
+    data['quantized_by'] = 'Jan'
 
-    # Write updated README.md
+    # Convert the updated data back to YAML
+    return yaml.safe_dump(data, sort_keys=False)
+
+def additional_content(model_id):
+    model_creator, model_name = model_id.split('/')
+    static_text =f"""
+<!-- header start -->
+<!-- 200823 -->
+<div style="width: auto; margin-left: auto; margin-right: auto">
+<img src="https://github.com/janhq/jan/assets/89722390/35daac7d-b895-487c-a6ac-6663daaad78e" alt="Jan banner" style="width: 100%; min-width: 400px; display: block; margin: auto;">
+</div>
+
+<p align="center">
+    <a href="https://jan.ai/">Jan</a> 
+    - <a href="https://discord.gg/AsJ8krTT3N">Discord</a>
+</p>
+<!-- header end -->
+
+# Model Description
+This is a GGUF version of [{model_id}](https://huggingface.co/{model_id})
+- Model creator: [{model_creator}](https://huggingface.co/{model_creator})
+- Original model: [{model_name}](https://huggingface.co/{model_id})
+- Model description: [Readme](https://huggingface.co/{model_id}/blob/main/README.md)
+
+# About Jan
+Jan believes in the need for an open-source AI ecosystem and is building the infra and tooling to allow open-source AIs to compete on a level playing field with proprietary ones.
+
+Jan's long-term vision is to build a cognitive framework for future robots, who are practical, useful assistants for humans and businesses in everyday life.
+
+# Jan Model Converter
+This is a repository for the opensource converter. We are grateful if community can contribute and strengthen this repo. We are aiming to expand the repo that can convert into various type of format
+"""
+    return static_text
+
+def update_readme():
+    """
+    Update the README.md file based on the environment variables.
+    """
+    model_id = os.getenv('MODEL_ID')
+    if not model_id:
+        raise ValueError("MODEL_ID environment variable is not set.")
+
+    updated_yaml = modify_metadata(model_id)
+    additional_md = additional_content(model_id)
+
+    # Reconstruct the README content
+    updated_content = '---\n' + updated_yaml + '---\n' + additional_md
+
+    # Write the updated content back to README.md
     with open(f'./${{ env.MODEL_NAME }}/README.md', 'w') as file:
-        file.write(updated_readme)
+        file.write(updated_content)
 
 if __name__ == '__main__':
-    fire.Fire(parse_metadata)
+    fire.Fire(update_readme)
