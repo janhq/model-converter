a962dfdab5efe1b79398b0d234653a29d1fc10e5
hiro <vuonghoainam.work@gmail.com>
2023-12-12 07:15:43 +0700

    fix: Update code for model card gen python

--- modelCardGen.py
+++ modelCardGen.py
@@ -2,37 +2,25 @@ import os
 import yaml
 import fire
 
-def modify_metadata(model_id):
+def modifyMetadata(modelId, modelAuthor, modelName):
     """
-    Modify the metadata of the README.md based on the model_id.
-    Args:
-    model_id (str): The model identifier in the format 'creator/name'.
-    Returns:
-    str: Updated YAML metadata as a string.
+    Update the metadata of the model.
     """
-    model_creator, model_name = model_id.split('/')
-
-    # Read the original README.md content
-    with open(f'./${{ env.MODEL_NAME }}/README.md', 'r') as file:
+    with open(f'./{modelName}/README.md', 'r') as file:
         content = file.read()
 
-    # Extract the YAML part from the content
     yaml_part = content.split('---')[1]
-
-    # Parse the YAML content
     data = yaml.safe_load(yaml_part)
 
-    # Update the YAML data with dynamic values
-    data['base_model'] = model_id
-    data['model_creator'] = model_creator
-    data['model_name'] = model_name
-    data['quantized_by'] = 'Jan'
+    data['base_model'] = modelId
+    data['model_creator'] = modelAuthor
+    data['model_name'] = modelName
+    data['quantized_by'] = 'JanHQ'
 
-    # Convert the updated data back to YAML
     return yaml.safe_dump(data, sort_keys=False)
 
-def additional_content(model_id):
-    model_creator, model_name = model_id.split('/')
+def modifyAdditionalMetadata(modelID, modelAuthor, modelName):
+    
     static_text =f"""
 <!-- header start -->
 <!-- 200823 -->
@@ -47,10 +35,10 @@ def additional_content(model_id):
 <!-- header end -->
 
 # Model Description
-This is a GGUF version of [{model_id}](https://huggingface.co/{model_id})
-- Model creator: [{model_creator}](https://huggingface.co/{model_creator})
-- Original model: [{model_name}](https://huggingface.co/{model_id})
-- Model description: [Readme](https://huggingface.co/{model_id}/blob/main/README.md)
+This is a GGUF version of [{modelID}](https://huggingface.co/{modelID})
+- Model creator: [{modelAuthor}](https://huggingface.co/{modelAuthor})
+- Original model: [{modelName}](https://huggingface.co/{modelID})
+- Model description: [Readme](https://huggingface.co/{modelID}/blob/main/README.md)
 
 # About Jan
 Jan believes in the need for an open-source AI ecosystem and is building the infra and tooling to allow open-source AIs to compete on a level playing field with proprietary ones.
@@ -62,23 +50,23 @@ This is a repository for the [open-source converter](https://github.com/janhq/mo
 """
     return static_text
 
-def update_readme():
+def update_readme(modelId):
     """
-    Update the README.md file based on the environment variables.
+    Update the README.md of the model folder.
     """
-    model_id = os.getenv('MODEL_ID')
-    if not model_id:
-        raise ValueError("MODEL_ID environment variable is not set.")
+    if not modelId:
+        raise ValueError("Please add ModelID.")
 
-    updated_yaml = modify_metadata(model_id)
-    additional_md = additional_content(model_id)
+    modelAuthor, modelName = modelId.split('/')
+    updated_yaml = modifyMetadata(modelId, modelAuthor, modelName)
+    additional_md = modifyAdditionalMetadata(modelId, modelAuthor, modelName)
 
-    # Reconstruct the README content
     updated_content = '---\n' + updated_yaml + '---\n' + additional_md
 
-    # Write the updated content back to README.md
-    with open(f'./${{ env.MODEL_NAME }}/README.md', 'w') as file:
+    with open(f'./{modelName}/README.md', 'w') as file:
         file.write(updated_content)
+    
+    print('README.md updated successfully.')
 
 if __name__ == '__main__':
     fire.Fire(update_readme)
